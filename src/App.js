b75fbb477d071cd43044f845bfab1dc74b3ebb9b
Prabhneet Singh <prabhneet13@gmail.com>
2022-09-14 20:55:18 +0530

    setting up .env

--- src/App.js
+++ src/App.js
@@ -2,10 +2,36 @@ import React, { useState, useEffect } from 'react'
 import { FaSearch } from 'react-icons/fa'
 import Photo from './Photo'
 // const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
-const mainUrl = `https://api.unsplash.com/photos/`
-const searchUrl = `https://api.unsplash.com/search/photos/`
+const clientId = `${process.env.REACT_APP_ACCESS_KEY}`
+const mainUrl = `https://api.unsplash.com/photos/?client_id=`
+const searchUrl = `https://api.unsplash.com/search/photos/?client_id=`
+// const accessKey = "1owf68QiVhmS_UaZqiGg5O_So_FjlpsBdlpaH3zpIK0"
 
 function App() {
+  const [loading, setloading] = useState(false);
+  const [photos, setphotos] = useState([]);
+
+  const fetchImages = async () => {
+    setloading(true);
+    let url;
+    url = `${mainUrl}${clientId}`;
+    try {
+      const response = await fetch(url);
+      const jsonData = await response.json();
+
+      setphotos(jsonData);
+      console.log(jsonData);
+    } catch (error) {
+      console.log(error);
+      setloading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
+
   return <h2>stock photos starter</h2>
 }
 
